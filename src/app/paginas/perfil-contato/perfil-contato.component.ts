d1fafa5b9f8e46c81179e0d341008fdd5f5b2d19
Henrique Lacerda <henriquelacerda69@gmail.com>
2025-07-07 16:00:04 -0300

    alterando perfil contato component

--- src/app/paginas/perfil-contato/perfil-contato.component.ts
+++ src/app/paginas/perfil-contato/perfil-contato.component.ts
@@ -1,24 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { ContainerComponent } from '../../componentes/container/container.component';
-import { SeparadorComponent } from "../../componentes/separador/separador.component";
+import { SeparadorComponent } from '../../componentes/separador/separador.component';
 import { CommonModule } from '@angular/common';
 import { Contato } from '../../componentes/contato/contato';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { ContatoService } from '../../services/contato.service';
-import { CabecalhoComponent } from "../../componentes/cabecalho/cabecalho.component";
-
-
-
+import { CabecalhoComponent } from '../../componentes/cabecalho/cabecalho.component';
 
 @Component({
   selector: 'app-perfil-contato',
   standalone: true,
   imports: [CommonModule, ContainerComponent, SeparadorComponent, RouterLink, CabecalhoComponent],
   templateUrl: './perfil-contato.component.html',
-  styleUrl: './perfil-contato.component.css'
+  styleUrl: './perfil-contato.component.css',
 })
-
-export class PerfilContatoComponent implements OnInit{
+export class PerfilContatoComponent implements OnInit {
   contato: Contato = {
     id: 0,
     avatar: '',
@@ -27,34 +23,40 @@ export class PerfilContatoComponent implements OnInit{
     email: '',
     aniversario: '',
     redes: '',
-    observacoes: ''
-  }
+    observacoes: '',
+  };
 
-  constructor(private activatedRoute: ActivatedRoute,
+  constructor(
+    private activatedRoute: ActivatedRoute,
     private contatoService: ContatoService,
     private router: Router
-  ){
-    
-  }
+  ) {}
 
-  ngOnInit()  {
-    const id = this.activatedRoute.snapshot.paramMap.get('id')
-    if(id){
-      this.contatoService.buscarPorId(parseInt(id)).subscribe((contato) =>{
-        this.contato = contato
-      }) 
+  ngOnInit(): void {
+    const idParam = this.activatedRoute.snapshot.paramMap.get('id');
+    const id = Number(idParam);
 
+    if (!isNaN(id)) {
+      this.contatoService.buscarPorId(id).subscribe((contato) => {
+        if (contato) {
+          this.contato = contato;
+        } else {
+          alert('Contato não encontrado.');
+          this.router.navigateByUrl('/lista-contatos');
+        }
+      });
+    } else {
+      console.warn('ID inválido na URL');
+      this.router.navigateByUrl('/lista-contatos');
     }
   }
 
-  excluir(){
-    if(this.contato.id){
+  excluir(): void {
+    if (this.contato?.id) {
       this.contatoService.excluirPorId(this.contato.id).subscribe(() => {
-        this.router.navigateByUrl('/lista-contatos')
-      })
-
+        alert('Contato excluído com sucesso.');
+        this.router.navigateByUrl('/lista-contatos');
+      });
     }
   }
-
- 
 }
